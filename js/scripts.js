4c13fbab4b683ba3f33dd040a2189fbc2639fdfe
MarlonVictor <cmarlonvictor11@gmail.com>
2022-02-14 17:58:42 -0300

    Aula 17: Configuring webpack and babel

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+import initModal from './modules/modal.js'
+import initTabNav from './modules/tabNav.js'
+import initTooltip from './modules/tooltip.js'
+import initAccordion from './modules/accordionAnimation.js'
+import initSmoothScroll from './modules/smoothScroll.js'
+import initDropdownMenu from './modules/dropdownMenu.js'
+import initScrollAnimation from './modules/scrollAnimation.js'
+import initMobileMenu from './modules/mobileMenu.js'
+import initOperationTime from './modules/operationTime.js'
+import initFetchAnimals from './modules/fetchAnimals.js'
+import initFetchBitcoin from './modules/fetchBitcoin.js'
+
+
+initModal()
+initTabNav()
+initTooltip()
+initAccordion()
+initSmoothScroll()
+initDropdownMenu()
+initScrollAnimation()
+initMobileMenu()
+initOperationTime()
+initFetchAnimals()
+initFetchBitcoin()
